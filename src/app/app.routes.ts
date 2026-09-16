4a5af35ee57d2eb3bc1241e5b850a9c6827546d1
Angello Contreras <angello.contreras29@gmail.com>
2026-09-15 21:03:25 -0600

    feat: add home and contact components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'contact', component: Contact },
+];
