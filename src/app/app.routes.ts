ce1f9006b2e3399c8971eb408499a9ba4fd6ae98
Angello Contreras <angello.contreras29@gmail.com>
2026-09-16 00:37:05 -0600

    feat: add recipes page with responsive design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Contact } from './pages/contact/contact';
+import { Recipes } from './pages/recipes/recipes';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'contact', component: Contact },
+  { path: 'recipes', component: Recipes },
 ];
